d4fba279f2f1985f5d8c6152ab0f50bc78a3ac7c
Petr-max41 <krilof25@mail.ru>
2024-09-24 17:04:32 +0500

    Add files via upload

---
+++ 'module_1_4'.py
@@ -0,0 +1,8 @@
+my_string = input('Сколько тебе лет: ')
+print('Привет,',my_string)
+my_string = 'Сколько тебе лет'
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+print(my_string [0])
+print(my_string[-1])
